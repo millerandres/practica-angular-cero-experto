2b59c637cd5c1a7a05deeffe5874110723ae72fa
MillerCaro <ingenierogom@gmail.com>
2024-07-08 14:50:46 -0500

    se separa la logica en subcomponentes dentro del modulo

--- bases/src/app/dbz/dbz.module.ts
+++ bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponentTsComponent } from './pages/main-page.component.ts/main-page.component.ts.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponentTsComponent
+    MainPageComponentTsComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   exports: [
     MainPageComponentTsComponent
