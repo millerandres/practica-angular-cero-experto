4409a206dd306d272376905f58988db59379decb
MillerCaro <ingenierogom@gmail.com>
2024-07-19 12:08:59 -0500

    se agrega validaciones al servicio

--- GifsApp/src/app/gifs/services/gifs.service.ts
+++ GifsApp/src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,23 @@ export class GifsService {
   }
 
   searchTag( tag:string ):void{
+
+    if(tag.length===0) return;
+    this.organizeHistory(tag)
     this._tagsHistory.unshift(tag)
     console.log(this._tagsHistory)
+  }
+
+  organizeHistory(tag:string){
+    tag = tag.toLowerCase()
+  
+    //incluye un nuevo registro solo si el registro es diferente de los que hay actualmente
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter( (oldtag) => oldtag !== tag )
+    
+    }
+
+    //Limitando el arreglo a 10 registros
+    this._tagsHistory = this._tagsHistory.splice(0,10);
   }
 }
